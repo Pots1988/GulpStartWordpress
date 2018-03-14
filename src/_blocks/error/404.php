d39e24bd76213051155b22def9986ab8cd969db6
pots1988bear@gmail.com <pots1988bear@gmail.com>
2018-03-14 10:23:25 +0200

    Default strucutre

---
+++ src/_blocks/error/404.php
@@ -0,0 +1,12 @@
+<?php get_header("simple"); ?>
+  <section class="error-page">
+    <div class="error-page__wrap">
+      <div class="error-page__content">
+        <h1 class="error-page__title">404</h1>
+        <p class="error-page__descr">Страница не найдена</p>
+        <a href="/" class="btn error-page__link">Перейдите на главную</a>
+        <p class="error-page__advice">или выберите подходящую категорию</p>
+      </div>
+    </div>
+  </section>
+<?php get_footer(); ?>
